545f4dd32bc0283c20b8ff6ffea159187af8e10d
Stephenson <salao@debt.com>
2022-01-03 05:56:56 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log('Hello ThreeJs World');
+console.log(THREE);
+
+//scene 
+const scene = new THREE.Scene();
+
+// red cube 
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 'red' });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const sizes = {
+  width: 800,
+  height: 600,
+}
+
+// camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+//renderer 
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
