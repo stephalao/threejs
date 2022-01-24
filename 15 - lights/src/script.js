f1b3f9a4eb79cccd6c0c7869574823e43a3e0d5d
Stephenson Alao <salao@debt.com>
2022-01-24 17:06:49 -0500

    15 - lights

--- 15 - lights/src/script.js
+++ 15 - lights/src/script.js
@@ -1,13 +1,16 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
+import lilGuiUmd, * as dat from 'lil-gui'
+import { AmbientLight, RectAreaLight, SpotLightHelper } from 'three'
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
+
 
 /**
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI({width: 400})
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -17,15 +20,58 @@ const scene = new THREE.Scene()
 
 /**
  * Lights
+ * Minimal Cost Lights - Hemisphere & Ambient
+ * Moderate Cost Lights - Directional & Point
+ * High Cost Lights - Spotlight & RectArea 
  */
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.01).name('Ambient Intensity')
 scene.add(ambientLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+const directionalLight = new THREE.DirectionalLight(0x0B2EF4, 0.4)
+directionalLight.position.set(1, .5, 0)
+gui.add(directionalLight, 'intensity').min(0).max(1).step(.01).name('Directional Intensity')
+scene.add(directionalLight)
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0B2EF4, .3)
+gui.add(hemisphereLight, 'intensity').min(0).max(1).step(.01).name('Hemisphere Intensity')
+scene.add(hemisphereLight)
+
+const pointLight = new THREE.PointLight(0xff9000, 0.5, 2)
+pointLight.position.set(1, -.5, 1)
+gui.add(pointLight, 'intensity').min(0).max(1).step(.01).name('Point Intensity')
 scene.add(pointLight)
+
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 3, 1)
+rectAreaLight.position.set(-1.5, 0, 1.5)
+rectAreaLight.lookAt(new THREE.Vector3())
+gui.add(rectAreaLight, 'intensity').min(0).max(4).step(.01).name('Rect Area Intensity')
+scene.add(rectAreaLight)
+
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * .1, .25, 1)
+spotLight.position.set(0, 2, 3)
+spotLight.target.position.x = -.75
+gui.add(spotLight, 'intensity').min(0).max(1).step(.01).name('Spotlight Intensity')
+scene.add(spotLight, spotLight.target)
+
+// Light Helpers 
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.1)
+// scene.add(hemisphereLightHelper)
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.1)
+scene.add(directionalLightHelper)
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.1)
+scene.add(pointLightHelper)
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight)
+scene.add(spotLightHelper)
+window.requestAnimationFrame(() =>{
+    spotLightHelper.update()
+})
+
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight)
+scene.add(rectAreaLightHelper)
 
 /**
  * Objects
