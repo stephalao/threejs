28a1e37d0521b50d096cb1d1bb8fad657bd44b3f
Stephenson Alao <salao@debt.com>
2022-02-01 17:12:14 -0500

    18 - particles

--- 18 - particles/src/script.js
+++ 18 - particles/src/script.js
@@ -19,6 +19,40 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/2.png')
+// console.log(particleTexture)
+
+// Particles
+//Geometry
+const particlesGeometry = new THREE.BufferGeometry(1, 32, 32)
+const count = 20000
+
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
+
+for(let i = 0; i < count * 3; i++){
+    positions[i] = (Math.random() - 0.5) * 10
+    colors[i] = Math.random()
+} 
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+
+// Material 
+const particleMaterial = new THREE.PointsMaterial({
+    size: 0.1,
+    vertexColors: true,
+    alphaMap: particleTexture,
+    depthWrite: false,
+    transparent: true,
+    sizeAttenuation: true
+})
+
+particleMaterial.blending = THREE.AdditiveBlending
+
+// Points 
+const particles = new THREE.Points(particlesGeometry, particleMaterial)
+scene.add(particles)
 
 /**
  * Test cube
@@ -27,7 +61,7 @@ const cube = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial()
 )
-scene.add(cube)
+// scene.add(cube)
 
 /**
  * Sizes
@@ -82,6 +116,19 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    // update particles 
+
+    // particles.rotation.y = elapsedTime * 0.2
+    // particles.position.y = - elapsedTime * 0.2
+
+    for(let i = 0; i < count; i++){
+        const i3 = i * 3
+        const x =particlesGeometry.attributes.position.array[i3]
+        
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    }
+    
+    particlesGeometry.attributes.position.needsUpdate = true
     // Update controls
     controls.update()
 
