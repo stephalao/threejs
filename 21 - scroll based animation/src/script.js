7a5b8c9e604af06eacc73d83c733cd90aafc66e0
Stephenson Alao <salao@debt.com>
2022-02-17 17:03:57 -0500

    21 - scroll animations

--- 21 - scroll based animation/src/script.js
+++ 21 - scroll based animation/src/script.js
@@ -13,6 +13,9 @@ const parameters = {
 
 gui
     .addColor(parameters, 'materialColor')
+    .onChange( () => {
+        material.color.set(parameters.materialColor)
+    })
 
 /**
  * Base
@@ -30,7 +33,50 @@ const cube = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
-scene.add(cube)
+// scene.add(cube)
+
+//Texture 
+const textureLoader = new THREE.TextureLoader()
+const gradientTexture = textureLoader.load('textures/gradients/3.jpg')
+gradientTexture.magFilter = THREE.NearestFilter
+
+// Objects 
+const material = new THREE.MeshToonMaterial({ 
+    color: parameters.materialColor,
+    gradientMap: gradientTexture
+})
+
+// Meshes
+const objectDistance = 4
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1, 0.4, 16, 60),
+    material
+)
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1, 2, 32),
+    material
+)
+const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+    material
+)
+mesh1.position.y = - objectDistance * 0
+mesh2.position.y = - objectDistance * 1
+mesh3.position.y = - objectDistance * 2
+
+mesh1.position.x = 2
+mesh2.position.x = - 2
+mesh3.position.x = 2
+
+scene.add(mesh1, mesh2, mesh3)
+
+const sectionMeshes = [mesh1, mesh2, mesh3]
+
+// Lights
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
+directionalLight.position.set(1, 1, 0)
+const DLhelper = new THREE.DirectionalLightHelper(directionalLight, 1)
+scene.add(directionalLight)
 
 /**
  * Sizes
@@ -67,10 +113,17 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// Scroll 
+let scrollY = window.scrollY
+window.addEventListener('scroll', () => {
+    scrollY = window.scrollY
+})
 
 /**
  * Animate
@@ -80,6 +133,15 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //Animate Camera 
+    camera.position.y = - scrollY / sizes.height * objectDistance
+
+    // Animate Meshes 
+    for( const mesh of sectionMeshes){
+        mesh.rotation.x = elapsedTime * 0.1
+        mesh.rotation.y = elapsedTime * 0.12
+    }  
 
     // Render
     renderer.render(scene, camera)
